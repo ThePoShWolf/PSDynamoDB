19dbca311f28248fe90df4dc08534ef71e0f3694
Anthony <anthony@howell-it.com>
2020-04-28 10:20:15 -0700

    now supports query and conversion

--- src/public/Get-DdbTableItem.ps1
+++ src/public/Get-DdbTableItem.ps1
@@ -1,14 +1,73 @@
 Function Get-DdbTableItem {
-    [cmdletbinding()]
+    [cmdletbinding(
+        DefaultParameterSetName = 'Single'
+    )]
     param (
+        [Parameter(
+            Mandatory,
+            ParameterSetName = 'Single'
+        )]
+        [Parameter(
+            ParameterSetName = 'IndexQuery'
+        )]
         [string]$HashKey,
-        [string]$RangeKey
+        [Parameter(
+            ParameterSetName = 'Single'
+        )]
+        [string]$RangeKey,
+        [Parameter(
+            ParameterSetName = 'IndexQuery'
+        )]
+        [string]$IndexName,
+        [Parameter(
+            ParameterSetName = 'IndexQuery'
+        )]
+        [int]$Limit,
+        [Parameter(
+            ParameterSetName = 'IndexQuery'
+        )]
+        [bool]$ScanIndexForward = $true
     )
-    if ($PSBoundParameters.Keys -contains 'RangeKey' ){
-        [System.Threading.Tasks.Task]$task = $table.GetItemAsync([Amazon.DynamoDBv2.DocumentModel.Primitive]::new($HashKey),[Amazon.DynamoDBv2.DocumentModel.Primitive]::new($RangeKey))
+    if ($PSCmdlet.ParameterSetName -eq 'Single') {
+        if ($PSBoundParameters.Keys -contains 'RangeKey' ){
+            $task = $table.GetItemAsync([Amazon.DynamoDBv2.DocumentModel.Primitive]::new($HashKey),[Amazon.DynamoDBv2.DocumentModel.Primitive]::new($RangeKey))
+        } else {
+            $task = $table.GetItemAsync([Amazon.DynamoDBv2.DocumentModel.Primitive]::new($HashKey))
+        }
+        $task.Wait()
+        $task.Result
     } else {
-        [System.Threading.Tasks.Task]$task = $table.GetItemAsync([Amazon.DynamoDBv2.DocumentModel.Primitive]::new($HashKey))
+        # Find the name of the hashkey
+        $hashKeyName = ($table.GlobalSecondaryIndexes[$IndexName].KeySchema | ?{$_.KeyType -eq 'HASH'}).AttributeName
+
+        $req = [Amazon.DynamoDBv2.Model.QueryRequest]::new()
+
+        # Really simple condition, hashkey = $hashkey
+        # This can be expanded
+        $condition = [Amazon.DynamoDBv2.Model.Condition]::new()
+        $condition.AttributeValueList.Add($HashKey)
+        $condition.ComparisonOperator = [Amazon.DynamoDBv2.ComparisonOperator]::EQ
+        $req.KeyConditions.Add($hashKeyName,$condition)
+
+        # Other parameters
+        $req.IndexName = $IndexName
+        $req.ScanIndexForward = $ScanIndexForward
+        $req.Limit = $Limit
+
+        # Since we are using the ddbClient to query, gotta specify tablename
+        $req.TableName = $table.TableName
+
+        # Query
+        $task = $ddbClient.QueryAsync($req)
+        $task.Wait()
+        
+        # Convert
+        foreach ($item in $task.Result.Items) {
+            $ht = @{}
+            foreach ($key in $item.Keys) {
+                $ht[$key] = ConvertFrom-AmazonDynamoDBv2AttributeValue $item[$key]
+            }
+            $ht
+        }
     }
-    $task.Wait()
-    $task.Result
 }
